3b011620bf69b1549eb325d6ac4ab886bb3bfbc1
Tuğrul Yıldırım <yazilimcitugrul@gmail.com>
2023-04-19 01:03:31 +0300

    str() and len() function and '+' operator

--- Day1/input_function.py
+++ Day1/input_function.py
@@ -5,3 +5,11 @@ input("Enter your name: ")
 # Use input function in the print function
 
 print("Hello " + input("Enter your name: "))
+
+# Print length of the input string
+# len() function is used to get the length of the string
+# str() function is used to convert the integer to string
+# Concatenate the string and integer using + operator
+print("Hello " + input("Enter your name: ") + " your name has " + str(len(input("Enter your name: "))) + " characters")
+
+# https://thonny.org/ python IDE for beginners
