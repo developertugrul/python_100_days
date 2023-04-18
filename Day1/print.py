f83d0a49d65073d683c1e297313661d15d4b4bd2
Tuğrul Yıldırım <yazilimcitugrul@gmail.com>
2023-04-19 00:47:19 +0300

    Print multiple lines

--- Day1/print.py
+++ Day1/print.py
@@ -6,3 +6,14 @@ print("Hello World")
 # "\" is used to print the double quotes in the string
 
 print("Hello \"World\"")
+
+# Print the string in multiple lines
+
+print('''
+  _    _      _ _                            _     _ _ 
+ | |  | |    | | |                          | |   | | |
+ | |__| | ___| | | ___   __      _____  _ __| | __| | |
+ |  __  |/ _ \ | |/ _ \  \ \ /\ / / _ \| '__| |/ _` | |
+ | |  | |  __/ | | (_) |  \ V  V / (_) | |  | | (_| |_|
+ |_|  |_|\___|_|_|\___/    \_/\_/ \___/|_|  |_|\__,_(_)
+''')
