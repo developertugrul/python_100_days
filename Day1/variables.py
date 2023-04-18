b9b0be0ba22a5483d319e08c1985922719760b3d
Tuğrul Yıldırım <yazilimcitugrul@gmail.com>
2023-04-19 01:18:11 +0300

    python variables

---
+++ Day1/variables.py
@@ -0,0 +1,21 @@
+# Python variables
+
+# String variable
+
+name = "Tuğrul"
+
+# Integer variable
+
+age = 25
+
+# Float variable
+
+height = 1.75
+
+# Boolean variable
+
+isStudent = True
+
+# Print variables
+
+print(name)
